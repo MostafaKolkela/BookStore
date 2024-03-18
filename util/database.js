7225876657ae8b16145e0d99bfc55b48a23efcd7
MostafaKolkela <115589891+MostafaKolkela@users.noreply.github.com>
2024-03-18 22:08:05 +0200

    Delete util/database.js

--- util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('book-store', 'root', 'Mostafa25102003', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
-
-module.exports = sequelize;
