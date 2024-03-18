56141674fc7c8c9a2b290f589d09c247b0695dce
MostafaKolkela <115589891+MostafaKolkela@users.noreply.github.com>
2024-03-18 17:44:02 +0200

    v.0.6

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ const sequelize = require('./util/database');
 //to send response to google book api
 const axios = require('axios')
 
-const API_KEY = 'AIzaSyCP8aFcdfSarTyzwObX0jhQm-xMgnCBT0M';
-const SEARCH_TERM = 'Harry Potter';
-LANG_RESTRICT = 'en'
+// const API_KEY = 'AIzaSyCP8aFcdfSarTyzwObX0jhQm-xMgnCBT0M';
+// const SEARCH_TERM = 'Harry Potter';
+// LANG_RESTRICT = 'en'
 
-axios.get(`https://www.googleapis.com/books/v1/volumes?q=${SEARCH_TERM}&langRestrict=${LANG_RESTRICT}&key=${API_KEY}`)
-  .then(response => {
-    const books = response.data.items;
-    books.forEach(book => {
-      console.log(book.volumeInfo);
-    });
-  })
-  .catch(err=> {
-    console.error('faild', err);
-  });
+// axios.get(`https://www.googleapis.com/books/v1/volumes?q=${SEARCH_TERM}&langRestrict=${LANG_RESTRICT}&key=${API_KEY}`)
+//   .then(response => {
+//     const books = response.data.items;
+//     books.forEach(book => {
+//       console.log(book.volumeInfo);
+//     });
+//   })
+//   .catch(err=> {
+//     console.error('faild', err);
+//   });
 
 sequelize
 .sync({force: true})
--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = require('../util/database');
 
-const user = Sequelize.define('user', {
+const user = sequelize.define('user', {
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
